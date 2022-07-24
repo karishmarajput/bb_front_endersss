e341ce88c239674917afd0b8e67b519492178bcc
Karishma <karrajput3948@gmail.com>
2022-07-24 21:05:54 +0530

    Add about us

--- src/components/elements/AboutUs.js
+++ src/components/elements/AboutUs.js
@@ -1,17 +1,17 @@
 import React from 'react';
-
+import './aboutUs.css';
 function AboutUs() {
   return (
   <div className="aboutUs">
   	<div className="aboutUs-main">
 	  	<div className="image-div">
 	  		        <img
-				  className="walter-pic"
+				  className="about-us-img"
 				  src={require('../../assets/images/home2.png')}
 				/>
 	  	</div>
 	  	<div className="about-info">
-	  	hello
+	  	You've<br/> Tried The Rest<br/> Now <br/>Try The Best 
 	  	</div>
   	</div>
   </div>
