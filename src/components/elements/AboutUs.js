5d53ce57aa4b458e119a572f0d21b21b544c2090
Karishma <karrajput3948@gmail.com>
2022-07-24 20:33:06 +0530

    correct navbar

--- src/components/elements/AboutUs.js
+++ src/components/elements/AboutUs.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
 function AboutUs() {
-  return <div></div>;
+  return (
+  <div className="aboutUs">
+  	<div className="aboutUs-main">
+	  	<div className="image-div">
+	  		        <img
+				  className="walter-pic"
+				  src={require('../../assets/images/home2.png')}
+				/>
+	  	</div>
+	  	<div className="about-info">
+	  	hello
+	  	</div>
+  	</div>
+  </div>
+  );
 }
-export default Video;
+export default AboutUs;
