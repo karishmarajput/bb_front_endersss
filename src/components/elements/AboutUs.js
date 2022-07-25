b2a00a3f50fc8c12ac8150bb05f9d3b4f7cd768b
Aaryan <aaryan24602@gmail.com>
2022-07-25 16:02:32 +0530

    ui updated

--- src/components/elements/AboutUs.js
+++ src/components/elements/AboutUs.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function AboutUs() {
   return (
-  <div className="aboutUs">
-  	<div className="aboutUs-main">
-	  	<div className="image-div">
-	  		        <img
-				  className="walter-pic"
-				  src={require('../../assets/images/home2.png')}
-				/>
-	  	</div>
-	  	<div className="about-info">
-	  	hello
-	  	</div>
-  	</div>
-  </div>
+    <div className="aboutUs">
+      <div className="aboutUs-main">
+        <div className="image-div">
+          <img
+            className="walter-pic"
+            src={require("../../assets/images/home2.png")}
+          />
+        </div>
+        <div className="about-info">hello</div>
+      </div>
+    </div>
   );
 }
 export default AboutUs;
