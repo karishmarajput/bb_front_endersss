80a6244e77b6e65066f7250bbab62b4db623fc3b
Aaryan <aaryan24602@gmail.com>
2022-07-26 17:06:27 +0530

    dark bg added

--- src/components/pages/Dark.js
+++ src/components/pages/Dark.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../elements/Header";
-import DarkHome from "../elements/Dark-home";
-
+import DarkHome from "../dark-elements/Dark-home";
+import "../dark-elements/Dark.css";
 export default function Dark() {
   return (
-    <div>
-      <Header />
+    <div className="dark-main-page">
       <DarkHome />
+      {/* <Header /> */}
     </div>
   );
 }
