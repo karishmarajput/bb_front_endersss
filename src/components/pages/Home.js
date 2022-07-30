167db294a8fa33d3010b5c5c2362a46009510565
Karishma <karrajput3948@gmail.com>
2022-07-30 21:19:56 +0530

    Delete files

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "../elements/Header.js";
 import Main from "../elements/Main.js";
 import Stats from "../elements/Stats.js";
-import Video from "../elements/Video.js";
 import About from "../elements/AboutUs.js";
 import BasicExample from "../elements/Form.js";
 import Footer from "../elements/Footer.js";
